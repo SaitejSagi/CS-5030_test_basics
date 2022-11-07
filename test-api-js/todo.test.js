6c656c7d7478087208499affe1d15d5324b849b0
SaiTej <saitej.sagi@slu.edu>
2022-11-07 21:38:15 +0530

    test

--- test-api-js/todo.test.js
+++ test-api-js/todo.test.js
@@ -24,4 +24,22 @@ describe('todo test suite', () => {
     // Write all your test cases here that corresponds to software requirements
 
 
+    test("add_todos", () => {
+        expect(todo_service.add_todo().todo.length).toEqual(4);
+    });
+
+  //deleting an element added in add_todos a deleted element is undefined
+    test("delete_todo(todo) ", () =>{
+        expect(todo_service.delete_todo().todo[3]).toEqual(undefined)
+    })
+
+    //updating an element and testing
+    test("update_todo(todo)", () =>{
+
+        expect(todo_service.update_todo().todo[2]).toEqual(
+            {"description": "D5", "done": false, "title": "T5"})
+    })
+
+
+
 });
